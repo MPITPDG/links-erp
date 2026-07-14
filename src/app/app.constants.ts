f32b37efd559be87a0823922d740a72eaac5dd5e
MPITPDG <sujit@manilal.com>
2026-07-14 18:32:38 +0530

    commit app.const

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-  public Server = 'http://localhost:58812//';   
-   ///  public Server = 'http://180.179.207.163/Links-api/';
+  //public Server = 'http://localhost:58812//';   
+    public Server = 'http://180.179.207.163/Links-api/';
   
     //public Server = 'http://180.179.207.163/Links-api/';    
     public ApiUrl = 'api/';
